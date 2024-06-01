6b5f96bdad595f93281f09cd06a3919688be544a
Isaac <isaacspirit333@gmail.com>
2024-06-01 18:52:16 +0200

    Optimize some code in Home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:decla/widgets/sidebar.dart';
+import 'package:decla/widgets/bottom_navigation.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -28,7 +29,7 @@ class HomePage extends StatelessWidget {
       ),
       drawer: const SidebarNavigation(),
       body: _buildUI(context),
-      bottomNavigationBar: _bottomNavigationBar(context),
+      bottomNavigationBar: BottomNavigation(),
     );
   }
 }
@@ -140,85 +141,60 @@ Widget _functionsButton(BuildContext context) {
               ],
             ),
           ),
-          SizedBox(width: 20), // Adjust the space between squares
-          Container(
-            width: 170,
-            height: 170,
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: const DecorationImage(
-                      image: AssetImage(
-                          'assets/images/stat.jpg'), // Add your background image
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius:
-                        BorderRadius.circular(10), // Add border radius
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(64, 135, 237, 1)
-                        .withOpacity(0.8), // Add color overlay with opacity
-                    borderRadius:
-                        BorderRadius.circular(10), // Add same border radius
-                  ),
-                ),
-                const Center(
-                  child: Text(
-                    'ُStats',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 4.0,
-                          color: Color.fromARGB(50, 0, 0, 0),
-                          offset: Offset(0, 4.0),
-                        ),
-                      ],
+          SizedBox(width: 20),
+          // Adjust the space between squares
+          GestureDetector(
+            onTap: () {
+              // Handle tap action here
+            },
+            child: Container(
+              width: 170,
+              height: 170,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage(
+                            'assets/images/stat.jpg'), // Add your background image
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius:
+                          BorderRadius.circular(10), // Add border radius
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(64, 135, 237, 1)
+                          .withOpacity(0.8), // Add color overlay with opacity
+                      borderRadius:
+                          BorderRadius.circular(10), // Add same border radius
+                    ),
+                  ),
+                  const Center(
+                    child: Text(
+                      'Stats',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 4.0,
+                            color: Color.fromARGB(50, 0, 0, 0),
+                            offset: Offset(0, 4.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
     ],
-  );
-}
-
-Widget _bottomNavigationBar(BuildContext context) {
-  return Container(
-    decoration: BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: const Color.fromARGB(10, 0, 0, 0).withOpacity(0.1),
-          spreadRadius: 5,
-          blurRadius: 8,
-          offset: Offset(0, 3), // changes position of shadow
-        ),
-      ],
-    ),
-    child: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Settings',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group),
-          label: 'Profile',
-        ),
-      ],
-    ),
   );
 }
---
+++ lib/widgets/bottom_navigation.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(10, 0, 0, 0).withOpacity(0.1),
+            spreadRadius: 5,
+            blurRadius: 8,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
