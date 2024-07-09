095b0653d5836ac85327034fc904e8b656172a3b
Isaac <isaacspirit333@gmail.com>
2024-07-09 09:18:00 +0200

    after firebase debuging

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Nardella\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Nardella\Documenti\Data Science\Mobile Programming and Multimedia\decla"
+export "FLUTTER_ROOT=D:\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter\MyflutterProjects\decla"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
